9c765352c610525cdc75dd0d157a9b83eb2333c1
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2024-12-07 19:44:01 +0530

    new

--- vending.sol
+++ vending.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity 0.8.28;
 
 contract vandingMachine{
     address public owner;
@@ -8,6 +8,7 @@ contract vandingMachine{
         owner = msg.sender;
         dounutBalance[address(this)] = 100;
     }
+
 
     function getVendingMachineBalance() public view returns (uint) {
         return dounutBalance[address(this)];
@@ -18,11 +19,13 @@ contract vandingMachine{
         require(msg.sender == owner, "Only the owner can restock this machine.");
         donutBalance[address(this)] += amount;
     }
+// new function a
+
     function purchase(uint amount) public payable{
         require(msg.value >= amount * 2 ether,"You must pay 2 ether per dountu");
         require(donutBalance[address(this)] >= amount, "There are not enough donuts available");
         donutBalance[address(this)] -= amount;
         donutBalance[msg.sender] += amount;
-    }
+    }//new funciton
 }
 
