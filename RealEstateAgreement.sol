ddf4df425c92312dd3fa501922fe9864e3e07b3a
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2024-12-23 19:14:13 +0530

    new

--- RealEstateAgreement.sol
+++ RealEstateAgreement.sol
@@ -3,5 +3,19 @@ pragma solidity 0.8.28;
 
 contract RealEstateAgreement{
     uint256 public price;
-    
+    bool public sellerPaysClosingFees;
+
+    constructor(uint256 _price) {
+        price = _price;
+        sellerPaysClosingFees = false;
+    }
+
+    function setPrice(uint _price) public{
+        price = _price;
+    }
+
+    function setClosingFeeAgreement(bool _ownerPays) public{
+        sellerPaysClosingFees = _ownerPays;
+    }
+
 }
