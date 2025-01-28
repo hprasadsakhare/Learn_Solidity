c5008c6b76beaad81ecbc608470bdab842a73b1d
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-01-28 17:30:30 +0530

    new

--- lottery.sol
+++ lottery.sol
@@ -1,48 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-contract lottery{
+contract Lottery {
     address public owner;
     address payable[] public players;
 
-    constructor(){
+    constructor() {
         owner = msg.sender;
     }
 
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
 
-function getBalance()public view returns(uint){
-    return address(this).balance;
-}
+    function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
 
-function getPlayer()public view return(address payable[] memory){
-    return players;
-}
-
-//enter function
-    function enter() public payable{
-        require(msg.value > .01 ether);
-        // address of player entering lottery
+    // Enter the lottery
+    function enter() public payable {
+        require(msg.value > 0.01 ether, "Minimum entry fee is 0.01 ether");
         players.push(payable(msg.sender));
     }
-    
-    function getRandomNumber() public view returns(uint){
+
+    // Generate a pseudo-random number (not secure for production)
+    function getRandomNumber() public view returns (uint) {
         return uint(keccak256(abi.encodePacked(owner, block.timestamp)));
     }
 
-    function pickWinner() public{
-       
+    // Pick a winner and transfer the balance
+    function pickWinner() public onlyOwner {
+        require(players.length > 0, "No players in the lottery");
         uint index = getRandomNumber() % players.length;
         players[index].transfer(address(this).balance);
 
-        //result the state of contract
-
+        // Reset the players array
         players = new address payable[](0);
-
     }
 
-    //new 
-    modifier onlyOwner(){
-        require(msg.sender == owner);
+    // Modifier to restrict access to the owner
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
         _;
     }
 }
