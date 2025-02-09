32d2e1f135b3cf2fc3fc2b20d4b51892578b3152
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-02-09 20:07:02 +0530

    new

--- allowance.sol
+++ allowance.sol
@@ -8,13 +8,15 @@ contract allowance{
     function checkBal() public view returns(uint){
         return address(this).balance;
     }
-//
-
 
     mapping(address => uint) public allowances;
     address public owner;
-// new
-    function addAllowances(address _to, uint amt)public{
 
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function addAllowances(address _to, uint amt) public{
+        require(owner == msg.sender,"not owner");
     }
 }
