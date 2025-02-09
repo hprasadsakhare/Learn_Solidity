edd7bc0cc8c6cde3f5a5a7feb68eaa0cca33f56f
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-02-09 20:29:31 +0530

    new

--- immutable.sol
+++ immutable.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.28;
 
 contract Immutable{
     address public immutable owner;
+    address public constant owner2 = address(1);
+    address public owner3 = address(1);
 
     constructor(address _owner){
         owner = _owner;
