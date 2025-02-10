c8315cfc3dfd1c8a52e05f75a609e4dab40d5c7e
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-02-10 15:30:46 +0530

    new

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -3,17 +3,33 @@ pragma solidity 0.8.28;
 
 contract CrowdFunding{
     address public owner;
-    mapping(address => uint) funders;
+    mapping(address => uint) public funders;
     uint public goal;
     uint public minAmount;
     uint public noOfFunders;
     uint public fundsRaised;
     uint public timePeriod;
 
-    constructor(uint _goal,uint _tiemPeriod){
+    constructor(uint _goal, uint _timePeriod){
         goal = _goal;
-        timePeriod = block.timestamp + _tiemPeriod;
+        timePeriod = block.timestamp + _timePeriod;
         owner = msg.sender;
         minAmount = 1000 wei;
+    }
+
+    function Contribute() public payable {
+        require(block.timestamp < timePeriod, "Funding Time is Over!");
+        require(msg.value >= minAmount, "Minimum amount criteria not satisfy");
+
+        if(funders[msg.sender] == 0){
+            noOfFunders++;
+        }
+
+        funders[msg.sender] += msg.value;
+        fundsRaised += msg.value;
+    }
+
+    receive() external payable {
+        Contribute();
     }
 }
