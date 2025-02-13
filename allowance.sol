2cc9e1e69a8ef1a82d044f80ffcc463a3db17bc4
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-02-14 00:27:28 +0530

    new

--- allowance.sol
+++ allowance.sol
@@ -1,22 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-contract allowance{
-    receive() payable external{
+contract Allowance {
+    receive() external payable {}
 
-    }
-    function checkBal() public view returns(uint){
+    function checkBal() public view returns (uint) {
         return address(this).balance;
     }
 
     mapping(address => uint) public allowances;
     address public owner;
 
-    constructor(){
+    constructor() {
         owner = msg.sender;
     }
 
-    function addAllowances(address _to, uint amt) public{
-        require(owner == msg.sender,"not owner");
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Not the owner");
+        _;
+    }
+
+    function addAllowances(address _to, uint amt) public onlyOwner {
+        allowances[_to] += amt;
+    }
+
+    function withdraw(uint amt) public {
+        require(allowances[msg.sender] >= amt, "Insufficient allowance");
+        require(address(this).balance >= amt, "Contract balance too low");
+        
+        allowances[msg.sender] -= amt;
+        payable(msg.sender).transfer(amt);
+    }
+
+    function reduceAllowance(address _to, uint amt) public onlyOwner {
+        require(allowances[_to] >= amt, "Allowance too low");
+        allowances[_to] -= amt;
+    }
+
+    function resetAllowance(address _to) public onlyOwner {
+        allowances[_to] = 0;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Invalid address");
+        owner = newOwner;
     }
 }
