60a7e8ff649b7897de371400772563611632b272
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-02-27 12:45:31 +0530

    new cdoe

---
+++ createProposal.sol
@@ -0,0 +1,16 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract ProposalContract {
+    struct Proposal {
+        string title;
+        address proposer;
+    }
+
+    Proposal[] public proposals;
+
+    function createProposal(string memory _title) public {
+        proposals.push(Proposal(_title, msg.sender));
+    }
+}
