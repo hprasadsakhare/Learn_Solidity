cc128dc662028693dfa7738f1a34b677a2469f00
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-03-07 17:30:28 +0530

    adding

--- airdroptoken.sol
+++ airdroptoken.sol
@@ -23,7 +23,9 @@ contract AirdropToken {
         IFakeToken _token,
         address[] memory _addressArray,
         uint256[] memory _amountArray
-    ) public {
+    )
+    
+     public {
         require(_addressArray.length == _amountArray.length, "Mismatched arrays");
 
         for (uint256 i = 0; i < _addressArray.length; i++) {
