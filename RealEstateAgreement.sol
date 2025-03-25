20a0a4a7969111481d856e0e085cbff6417230a8
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-03-26 05:05:38 +0530

    adding

--- RealEstateAgreement.sol
+++ RealEstateAgreement.sol
@@ -11,6 +11,7 @@ contract RealEstateAgreement {
         price = _price;
         sellerPaysClosingFees = false;
     }
+    
 // new code is now issue described 
     receive() external payable {} // ether to gwi
     fallback() external payable {} // ether + data to contract 
