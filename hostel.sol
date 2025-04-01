6cc42d8a03b11c1d8ff386f06d93469df5e69fa9
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-04-01 18:47:15 +0530

    new code

---
+++ hostel.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Hostel{
+
+    address payable tenant;
+    address payable landlored;
+
+    uint public no_of_rooms = 0;
+    uint public no_of_agreement = 0;
+    uint public no_of_rent = 0;
+
+    struct Room{
+        uint roomid;
+        uint agreementid;
+        string roomname;
+        string roomaddress;
+        uint rent_per_minth;
+        uint securityDeposit;
+        uint timestamp;
+        bool vacant;
+        address payable landlord;
+        address payable currentTenant;
+    }
+}
