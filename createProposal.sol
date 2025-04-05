4468875f68da0a80421058302101f0a7300cf6f8
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-04-06 00:38:11 +0530

    new code

--- createProposal.sol
+++ createProposal.sol
@@ -5,7 +5,9 @@ contract ProposalContract {
         string title;
         address proposer;
     }
+    
     Proposal[] public proposals;
+
 
     function createProposal(string memory _title) public {
         proposals.push(Proposal(_title, msg.sender));
