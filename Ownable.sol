d6e3765d1db2e808a460fc6703d78f4e82ada56e
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-04-14 12:19:51 +0530

    new contract added

--- Ownable.sol
+++ Ownable.sol
@@ -13,6 +13,7 @@ contract Ownable {
         _;
     }
 
+
     function setOwner(address _newOwner) external onlyOwner {
         require(_newOwner != address(0), "invalid address");
         owner = _newOwner;
