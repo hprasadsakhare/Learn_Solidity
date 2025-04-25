e8c48853d8c10859f7c0d380f19454119fa8422d
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-04-26 00:24:11 +0530

    new changes

--- greeting.sol
+++ greeting.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-
 contract Greeting{
     string public name;
     string public greetingPrefix = "Hello";
     //ftradce
-
     constructor(string memory initialName){
         name = initialName;
     }
@@ -17,5 +15,5 @@ contract Greeting{
     function getGreeting() public view returns (string memory) {
         return string(abi.encodePacked(greetingPrefix, name));
     }
-    
+
 }
