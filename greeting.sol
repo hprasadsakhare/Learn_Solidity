bb67a72ac6597ea10f6aecd0a541166ae5f75428
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-05-03 00:19:31 +0530

    new code updated

--- greeting.sol
+++ greeting.sol
@@ -9,10 +9,10 @@ contract Greeting{
     constructor(string memory initialName){
         name = initialName;
     }
+    
     function setName(string memory newName) public {
         name = newName;
     }
-
 
     function getGreeting() public view returns (string memory) {
         return string(abi.encodePacked(greetingPrefix, name));
