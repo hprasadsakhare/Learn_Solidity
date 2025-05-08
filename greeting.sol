94100a19a9b3ac036568737f3423a1261c248504
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-05-09 00:18:19 +0530

    new

--- greeting.sol
+++ greeting.sol
@@ -15,7 +15,6 @@ contract Greeting{
         name = newName;
     }
     
-
     function getGreeting() public view returns (string memory) {
         return string(abi.encodePacked(greetingPrefix, name));
     }
