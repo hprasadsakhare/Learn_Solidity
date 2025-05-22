0340672759abd475de8e98e55682301e32285d45
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-05-22 13:49:04 +0530

    new

--- Decentralized_Marketplace.sol
+++ Decentralized_Marketplace.sol
@@ -44,6 +44,7 @@ contract DecentralizedMarketplace {
         
         emit ItemListed(itemCount, _name, _price, _quantity, msg.sender);
     }
+    
 
     function buyItem(uint _id, uint _quantity) external payable {
         Item storage item = items[_id];
