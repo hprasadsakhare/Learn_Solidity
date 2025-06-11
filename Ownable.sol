12f83778dc60086e88ec0e29098d82f93050a050
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-06-11 23:44:36 +0530

    n

--- Ownable.sol
+++ Ownable.sol
@@ -12,6 +12,7 @@ contract Ownable {
         require(msg.sender == owner, "not owner");
         _;
     }
+    
 
     function setOwner(address _newOwner) external onlyOwner {
         require(_newOwner != address(0), "invalid address");
