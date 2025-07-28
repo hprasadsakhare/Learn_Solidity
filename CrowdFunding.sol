5422cddf271b36a2d22ffd2c90c28506a2a33f50
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-07-28 17:34:44 +0530

    new changes

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -60,7 +60,6 @@ contract CrowdFunding {
         _;
     }
 
-
     function createRequest(string memory _description, uint _amount, address payable _receiver) isOwner public {
         Requests storage newRequest = AllRequests[numReq];
         numReq++;
